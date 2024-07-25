007da942b3161951b031f030569d46c61f9f6769
Mustafa Gh <mustafa@abacus.co>
2024-07-25 16:29:20 +1000

    update

--- lib/video_chat_screen.dart
+++ lib/video_chat_screen.dart
@@ -127,7 +127,12 @@ class _VideoChatScreenState extends State<VideoChatScreen> {
 
     _peerConnection = await webrtc.createPeerConnection({
       'iceServers': [
-        {'urls': 'stun:stun.l.google.com:19302'},
+        {
+          'urls': [
+            'stun:stun1.l.google.com:19302',
+            'stun:stun2.l.google.com:19302'
+          ]
+        },
       ]
     });
 
